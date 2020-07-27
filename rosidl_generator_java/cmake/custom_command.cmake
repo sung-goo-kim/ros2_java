3e1a3a375a268a4038c64c8a46cb7fe92dd004b5
Jacob Perron <jacob@openrobotics.org>
2020-07-27 12:22:20 -0700

    Update copyright notices
    Update all copyright notices to have the generic notice
    
    "Copyright <year> the ros2-java contributors"
    
    This is to make it clear that the project is not owned by any particular organization and reduces burden on maintaining multiple copyright notices per file.
    
    Related discussion: https://github.com/ros2-java/ros2_java/blob/cf2ef0707f7420f9b83e6c053775c9d52752a9e2/rcljava/include/org_ros2_rcljava_client_ClientImpl.h#L1-L13
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- rosidl_generator_java/cmake/custom_command.cmake
+++ rosidl_generator_java/cmake/custom_command.cmake
@@ -1,4 +1,4 @@
-# Copyright 2017 Esteve Fernandez <esteve@apache.org>
+# Copyright 2017 the ros2-java contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
